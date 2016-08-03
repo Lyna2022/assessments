613431f3d023a49cc16d49091fd12a624d0b9b56
ssobie <ssobie@uvic.ca>
2016-08-03 15:05:17 -0700

    Cleaned up the extraction script and simplified function calls.

--- extract.bccaq.gcm.r
+++ extract.bccaq.gcm.r
@@ -5,20 +5,14 @@
 library(ncdf4)
 library(PCICt)
 
-extract.gcm <- function(gcm,rcm=NULL,var.name,scenario,
+extract.gcm <- function(gcm,rcm=NULL,var.name,scenario,type,
+	   		lon.bnds,lat.bnds,past.int,proj.int,
                         data.dir,write.dir) {
-bccaq <- TRUE
-run.gcm <- FALSE
-anusplin <- FALSE
-prism <- FALSE
-
-  if (is.null(rcm)) {
-    
+   
     ##BCCAQ GCM Files
-    if (bccaq==TRUE) {
-      ##file.match <- list.files(path=paste(data.dir,gcm,sep=''),pattern=var.name)[1]
+    if (type=='BCCAQ') {
+
       file.name <- list.files(path=data.dir,pattern=paste(gcm,'_historical\\+',scenario,'_*',sep=''),full.name=TRUE)[1]
-      ##file.name <- list.files(path=data.dir,pattern=paste('*bccaq\\+',gcm,'_historical\\+',scenario,'_*',sep=''),full.name=TRUE)[1]
 
       file.split <- strsplit(file.name,'_')[[1]]
       run <- file.split[grep('r*i1p1',file.split)]
@@ -31,120 +25,43 @@ prism <- FALSE
       ##run <- strsplit(file.name,'_')[[1]][5]      
       ##file.name <- paste(data.dir,'pr+tasmax+tasmin_day_BCCAQ+ANUSPLIN300+NCEP1_1948-1990+1991-2011_evaluation_19480101-20111231.nc',sep='')
       ##paste(data.dir,'pr+tasmax+tasmin_day_ANUSPLIN300_observation_v20130130_19500101-20101231.nc',sep='')
-        
-      ##run <- 'base'
-      ##write.hist.name <- paste(var.name,'_day_BCCAQ_',gcm,'_',scenario,'_',run,'_1951-2000.nc',sep='')
-      ##write.proj.name <- paste(var.name,'_day_BCCAQ_',gcm,'_',scenario,'_',run,'_2001-2100.nc',sep='')
-      
+             
       #lon.offset <- 0
-      write.hist.name <- paste(var.name,'_day_BCCAQ_',gcm,'_',scenario,'_',run,'_1951-2000.nc',sep='') ###
-      write.proj.name <- paste(var.name,'_day_BCCAQ_',gcm,'_',scenario,'_',run,'_2001-2100.nc',sep='')
+      pys <- strsplit(past.int,'-')[[1]]
+      fys <- strsplit(proj.int,'-')[[1]]
 
-      ##BC Boundaries
-      ##      lon.bnds <- c(-140,-114)
-      ##      lat.bnds <- c(48,60)
-      ##MOTI Boundaries
-      ##lon.bnds <- c(-127.0,-126.0)
-      ##lat.bnds <- c(52,53)
-      ##Metro Vancouver Boundaries
-      ##lon.bnds <- c(-123.5,-122.4)
-      ##lat.bnds <- c(49,49.6)
-      ##PNW Boundaries for US
-      ##lon.bnds <- c(-125,-114)
-      ##lat.bnds <- c(41.5,49)
-      ##Metro Van/Whistler Region Boundaries
-      ##lon.bnds <- c(-123.51,-120.69)
-      ##lat.bnds <- c(48.99,50.41)
-      ##Okanagan Region Boundaries
-      ##lon.bnds <- c(-121.2,-118.025)
-      ##lat.bnds <- c(48.99,51.1)
-      ##Nanaimo Hospital Boundaries
-      ##lon.bnds <- c(-124.30,-123.70)
-      ##lat.bnds <- c(48.86,49.33)
-      ##South Vancouver Island
-      lon.bnds <- c(-124.60,-122.95)
-      lat.bnds <- c(48.25,49.2)
+      write.hist.name <- paste(var.name,'_day_BCCAQ_',gcm,'_',scenario,'_',run,'_'pys[1],'-',pys[2],'.nc',sep='') ###
+      write.proj.name <- paste(var.name,'_day_BCCAQ_',gcm,'_',scenario,'_',run,'_',fys[1],'-',fys[2],'.nc',sep='')
+
       hist.dir <- paste(write.dir,gcm,'/',sep='')
       if (!file.exists(hist.dir))
         dir.create(hist.dir,recursive=TRUE)      
     }
     
     ##-----------------------------------
-    if (run.gcm==TRUE) {
-      ##GCM files - search through the main CMIP5 climate directory for files
-      file.name <- list.files(path=paste(data.dir,gcm,'/',sep=''),pattern=paste('*',var.name,'_day_',gcm,'_historical\\+',scenario,'_*',sep=''),full.name=TRUE)[1]    
 
-      file.split <- strsplit(file.name,'_')[[1]]
-      run <- file.split[grep('r*i1p1',file.split)]
-
-      write.hist.name <- paste(var.name,'_day_',gcm,'_',scenario,'_',run,'_1971-2000.nc',sep='')
-      write.proj.name <- paste(var.name,'_day_',gcm,'_',scenario,'_',run,'_2041-2070.nc',sep='')
-
-      lon.offset <- 360
-      lon.bnds <- c(-145,-110)
-      lat.bnds <- c(45,65)
-      hist.dir <- paste(write.dir,gcm,'/',sep='')
-      if (!file.exists(hist.dir))
-        dir.create(hist.dir,recursive=TRUE)
-      browser()
-    }
-    if (anusplin==TRUE) {
+    if (type=='ANUSPLIN') {
       file.name <- paste(data.dir,'pr+tasmax+tasmin_day_ANUSPLIN300_observation_v20141027_19500101-20121231.nc',sep='')
-      ##paste(data.dir,'pr+tasmax+tasmin_day_ANUSPLIN300_observation_v20130130_19500101-20101231.nc',sep='')
       run <- 'v2013'
       lon.offset <- 0
       write.hist.name <- paste(var.name,'_day_ANUSPLIN_observation_',run,'_1950-1992.nc',sep='')
       write.proj.name <- NULL
-      ##Health impacts boundaries ##BC Boundaries
-      lon.bnds <- c(-123.303,-121.236) ##c(-140,-114)
-      lat.bnds <- c(48.989,50.5197) ##c(48,60)
       hist.dir <- write.dir
       if (!file.exists(hist.dir))
         dir.create(hist.dir,recursive=TRUE)
 
     }
-    if (prism==TRUE) {
+    if (type=='PRISM') {
       file.name <- paste(data.dir,var.name,'_monClim_PRISM_historical_run1_197101-200012.nc',sep='')
       lon.offset <- 0
       write.hist.name <- paste(var.name,'_day_PRISM_observation_1971-2000.nc',sep='')
       write.proj.name <- NULL
-      ##Health impacts boundaries ##BC Boundaries
-      ##lon.bnds <- c(-123.303,-121.236) ##c(-140,-114)
-      ##lat.bnds <- c(48.989,50.5197) ##c(48,60)
-      ##Metro Van/Whistler Region Boundaries
-      lon.bnds <- c(-123.51,-120.69)
-      lat.bnds <- c(48.99,50.41)
-      ##Okanagan Region Boundaries
-      ##lon.bnds <- c(-121.2,-118.1)
-      ##lat.bnds <- c(48.99,51.1)
-      ##Nanaimo Hospital Boundaries
-      ##lon.bnds <- c(-124.24,-123.70)
-      ##lat.bnds <- c(48.95,49.33)
-      ##South Vancouver Island
-      ##lon.bnds <- c(-124.60,-122.95)
-      ##lat.bnds <- c(48.25,49.2)
+
       
       hist.dir <- write.dir
       if (!file.exists(hist.dir))
         dir.create(hist.dir,recursive=TRUE)
     }    
-  } else {
-    file.name <- list.files(path=data.dir,pattern=paste('*',gcm,'\\+',rcm,'_historical\\+',scenario,'_*',sep=''),full.name=TRUE)[1]
-
-    file.split <- strsplit(file.name,'_')[[1]]
-    run <- file.split[grep('r*i1p1',file.split)]
-
-    lon.offset <- 0
-    write.hist.name <- paste(var.name,'_day_BCCAQ_',gcm,'_',rcm,'_1971-2000.nc',sep='')
-    write.proj.name <- paste(var.name,'_day_BCCAQ_',gcm,'_',rcm,'_2041-2070.nc',sep='')
-    lon.bnds <- c(-140,-114)
-    lat.bnds <- c(48,60)
-    hist.dir <- paste(write.dir,gcm,'_',rcm,'/',sep='')
-    if (!file.exists(hist.dir))
-      dir.create(hist.dir,recursive=TRUE)    
-  }
-
-  print(file.name)
 
 ##--------------------------------------------------------------
 
@@ -157,30 +74,24 @@ prism <- FALSE
   origin.pcict <- as.PCICt(strsplit(time.units, ' ')[[1]][3],
                            cal=time.calendar)
   time.series <- origin.pcict + time.values*86400
+  pys <- strsplit(past.int,'-')[[1]]
+  fys <- strsplit(proj.int,'-')[[1]]
 
-  if (is.null(rcm)) {
-    if (bccaq) {
-      past.st <- head(grep('1951-*',time.series),1) ###
-      past.en <- tail(grep('2000-*',time.series),1)
+  if (bccaq) {
+    past.st <- head(grep(paste(pys[1],'-*',sep=''),time.series),1) ###
+    past.en <- tail(grep(paste(pys[2],'-*',sep=''),time.series),1)
 
-      proj.st <- head(grep('2001-*',time.series),1)
-      ##proj.en <- tail(grep('2100-*',time.series),1)
-      ##proj.st <- 1 ##head(grep('1991-*',time.series),1)
-      proj.en <- length(time.series) ##tail(grep('2100-*',time.series),1)      
-    } else if (prism) {
-      past.st <- 1
-      past.en <- 12
-      proj.st <- 1
-      proj.en <- 12
-    }
-    
-  } else {
-    past.st <- head(grep('1971-*',time.series),1)
-    past.en <- tail(grep('2000-*',time.series),1)
-    
-    proj.st <- head(grep('2041-*',time.series),1)
-    proj.en <- tail(grep('2070-*',time.series),1)
+    proj.st <- head(grep(paste(fys[1],'-*',sep=''),time.series),1)
+    ##proj.en <- tail(grep('2100-*',time.series),1)
+    ##proj.st <- 1 ##head(grep('1991-*',time.series),1)
+    proj.en <- length(time.series) ##tail(grep('2100-*',time.series),1)      
+  } else if (prism) {
+    past.st <- 1
+    past.en <- 12
+    proj.st <- 1
+    proj.en <- 12
   }
+    
   
 if (length(past.en)==0)
   past.en <- proj.st-1
@@ -365,7 +276,35 @@ fix.precipitation <-  function(gcm,rcm) {
 
 ##**************************************************************************************
 
+      ##BC Boundaries
+      ##      lon.bnds <- c(-140,-114)
+      ##      lat.bnds <- c(48,60)
+      ##MOTI Boundaries
+      ##lon.bnds <- c(-127.0,-126.0)
+      ##lat.bnds <- c(52,53)
+      ##Metro Vancouver Boundaries
+      ##lon.bnds <- c(-123.5,-122.4)
+      ##lat.bnds <- c(49,49.6)
+      ##PNW Boundaries for US
+      ##lon.bnds <- c(-125,-114)
+      ##lat.bnds <- c(41.5,49)
+      ##Metro Van/Whistler Region Boundaries
+      ##lon.bnds <- c(-123.51,-120.69)
+      ##lat.bnds <- c(48.99,50.41)
+      ##Okanagan Region Boundaries
+      ##lon.bnds <- c(-121.2,-118.025)
+      ##lat.bnds <- c(48.99,51.1)
+      ##Health impacts boundaries ##BC Boundaries
+      ##lon.bnds <- c(-123.303,-121.236)
+      ##lat.bnds <- c(48.989,50.5197)
+      ##Nanaimo Hospital Boundaries
+      ##lon.bnds <- c(-124.30,-123.70)
+      ##lat.bnds <- c(48.86,49.33)
+      ##South Vancouver Island
+      lon.bnds <- c(-124.60,-122.95)
+      lat.bnds <- c(48.25,49.2)
 
+##**************************************************************************************
 
 gcm.list <- c('ACCESS1-0',
               'CCSM4',
@@ -380,75 +319,22 @@ gcm.list <- c('ACCESS1-0',
               'MPI-ESM-LR',
               'MRI-CGCM3')
 
-
-  rcm.list <- list(c('CCSM','CRCM'),
-                   c('CCSM','MM5I'),
-                   c('CCSM','WRFG'),
-                   c('CGCM3','CRCM'),
-                   c('CGCM3','RCM3'),
-                   c('CGCM3','WRFG'),
-                   c('GFDL','ECP2'),
-                   c('GFDL','HRM3'),
-                   c('GFDL','RCM3'),
-                   c('HADCM3','HRM3'),
-                   c('HADCM3','MM5I'))
-
 extract.bccaq.gcms <- function() {
   data.dir <- '/storage/data/climate/downscale/CMIP5/BCCAQ/'
-    ##'/storage/data/projects/rci/data/stat.downscaling/BCCAQ/bccaq_gcm_bc_subset/rcp85/'
-  ##'/storage/data/climate/ANUSPLIN/ANUSPLIN300_DAILY_STANDARD_GRIDS/'
-  ##'/storage/data/climate/downscale/rean/BCCAQ/'    ## '/storage/data/climate/downscale/CMIP5/nobackup/CONUS/' ##
   write.dir <- '/storage/data/scratch/ssobie/bccaq_gcm_south_island_subset/'
-    ##'/storage/data/scratch/ssobie/bccaq_gcm_metro_van_subset/'##
-  ##gcm.list <- 'ACCESS1-0'
   var.list <- c('pr','tasmax','tasmin')
   scenario <- 'rcp85'
+  past.int <- '1951-2000'
+  proj.int <- '2001-2100'
 
-##  if (var.name != 'pr')
-##    stop('Check adjustment for temperatures')
-for (var.name in var.list) {
-  for (model in gcm.list) {
-    gcm <- model[1]
-    rcm <- NULL 
-    test <- extract.gcm(gcm,rcm,var.name,scenario,
+  for (var.name in var.list) {
+    for (model in gcm.list) {
+      gcm <- model[1]
+      rcm <- NULL 
+      test <- extract.gcm(gcm,rcm,var.name,scenario,type='BCCAQ',
+			lon.bnds,lat.bnds,past.int,proj.int,
                         data.dir,write.dir)
-    
-  }
-}
-}
-
-extract.gcms <- function() {
-  ##data.dir <- '/storage/data/climate/downscale/CMIP5/anusplin_downscaling_cmip5/preprocess/CMIP5/'
-  data.dir <- '/storage/data/climate/CMIP5/output/'
-  write.dir <- '/storage/data/projects/rci/data/stat.downscaling/scaling_comparison/gcm_bc_subset/'
-
-  var.name <- 'tasmax'
-  scenario <- 'rcp45'
-  if (var.name != 'pr')
-    stop('Check adjustment for temperatures')
-
-  for (model in gcm.list) {
-    gcm <- model[1]
-    rcm <- NULL 
-    test <- extract.gcm(gcm,rcm,var.name,scenario,
-                        data.dir,write.dir)
-  }
-}
-
-extract.bccaq.rcms <- function() {
-  data.dir <- '/storage/data/climate/downscale/NARCCAP/BCCAQ/'
-##  write.dir <- '/storage/data/projects/rci/data/stat.downscaling/scaling_comparison/bccaq_rcm_bc_subset/'
-  write.dir <- '/storage/data/scratch/ssobie/bccaq_rcm_bc_subset/'
-
-  var.name <- 'tasmin'
-  scenario <- 'sresa2'
-  
-  for (model in rcm.list) {
-    gcm <- model[1]
-    rcm <- model[2]
-    test <- extract.gcm(gcm,rcm,var.name,scenario,
-                        data.dir,write.dir)
-
+    }
   }
 }
 
@@ -459,20 +345,24 @@ extract.anusplin <- function() {
   var.name <- 'tasmin'
   scenario <- 'observation'  
 
-  test <- extract.gcm(gcm='ANUSPLIN',rcm=NULL,var.name,scenario,
+  test <- extract.gcm(gcm='ANUSPLIN',rcm=NULL,var.name,scenario,type='ANUSPLIN',
+ 		      lon.bnds,lat.bnds,past.int,proj.int,
                       data.dir,write.dir)
 }
 
 extract.prism <- function() {
   data.dir <- '/storage/data/climate/PRISM/dataportal/'
   write.dir <- '/storage/data/scratch/ssobie/bccaq_gcm_van_whistler_subset/PRISM/'
-  var.name <- 'tmin'
+  var.list <- c('pr','tmax','tmin')
   scenario <- 'observation'  
-
-  test <- extract.gcm(gcm='PRISM',rcm=NULL,var.name,scenario,
-                      data.dir,write.dir)
+  for (var.name in var.list) {
+    test <- extract.gcm(gcm='PRISM',rcm=NULL,var.name,scenario,type='PRISM',
+			lon.bnds,lat.bnds,past.int,proj.int,
+                        data.dir,write.dir)
+  }
 }
 
+##--------------------------------------------------------------
 
 extract.bccaq.gcms()  
 ##extract.prism()
